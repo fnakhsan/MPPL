b60cfb0cdbb4823070b123bb1586a36e35d9f7e8
fnakhsan <fn.akhsan@gmail.com>
2022-05-18 13:23:44 +0700

    fix login 2

--- main/main.php
+++ main/main.php
@@ -1,3 +1,12 @@
+<?php
+// session_start();
+// if (!isset($_SESSION['session_username'])) {
+//     header("location:login.php");
+//     exit();
+// }
+// print_r($_SESSION);
+// print_r($_COOKIE); ?>
+
 <!DOCTYPE html>
 <html lang="en">
 
