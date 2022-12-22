3f27c0f274dc61a1c646721887faa7fc92573f86
fnakhsan <fn.akhsan@gmail.com>
2022-12-22 16:44:07 +0700

    MANTAB

--- admin/admin.php
+++ admin/admin.php
@@ -1,6 +1,11 @@
 <?php
 session_start();
-if (!isset($_SESSION['session_username']) && $_SESSION['session_account'] != "Admin") {
+if (!isset($_SESSION['session_username'])) {
+    header("location:../index.php");
+    exit();
+}
+
+if (($_SESSION['session_account'] != "Admin")) {
     header("location:../index.php");
     exit();
 }
@@ -16,6 +21,26 @@ $today =  date("Y-m-d");
 $query1 = mysqli_query($koneksi, "SELECT * FROM presensi_guru where tgl_presensi = '$today' ORDER BY id_presensi ASC");
 $query2 = mysqli_query($koneksi, "SELECT * FROM presensi_siswa where tgl_presensi = '$today' ORDER BY id_presensi ASC");
 
+if (isset($_POST['submitBtn'])) {
+    while ($data = mysqli_fetch_array($query2)) {
+        if (isset($_POST['validate' . $data['id_presensi']]) && isset($_POST['id_presensi' . $data['id_presensi']])) {
+            mysqli_query($koneksi, "update presensi_siswa set valid = '{$_POST['validate' .$data['id_presensi']]}' where id_presensi = '{$_POST['id_presensi' .$data['id_presensi']]}'");
+            $alert = "Presensi siswa berhasil dilakukan";
+        } else {
+            $alert = "Presensi siswa tidak berhasil dilakukan";
+        }
+    }
+    while ($data = mysqli_fetch_array($query1)) {
+        if (isset($_POST['validate' . $data['id_presensi']]) && isset($_POST['id_presensi' . $data['id_presensi']])) {
+            mysqli_query($koneksi, "update presensi_guru set valid = '{$_POST['validate' .$data['id_presensi']]}' where id_presensi = '{$_POST['id_presensi' .$data['id_presensi']]}'");
+            $alert = "Presensi guru berhasil dilakukan";
+        } else {
+            $alert = "Presensi guru tidak berhasil dilakukan";
+        }
+    }
+
+    header("location:../history-admin/history.php");
+}
 ?>
 
 <html lang="en">
@@ -51,43 +76,54 @@ $query2 = mysqli_query($koneksi, "SELECT * FROM presensi_siswa where tgl_presens
                 </ul>
             </div>
         </div>
-        <form action="/action_page.php">
-            <div class="main">
-                <div class="title">
-                    <h2>Presensi Hari Ini</h2>
-                </div>
-                <div class="today">
-                    <h3><?php echo $today; ?></h3>
-                </div>
-                <table class="table" border="1">
-                    <thead>
-                        <tr>
-                            <th>NIS / NIP</th>
-                            <th>Status</th>
-                            <th>keterangan</th>
-                            <th>Validasi</th>
-                        </tr>
-                    </thead>
-                    <tbody>
+        <div class="main">
+            <div class="title">
+                <h2>Presensi Hari Ini</h2>
+            </div>
+            <div class="today">
+                <h3><?php echo $today; ?></h3>
+            </div>
+            <table class="table" border="1">
+                <thead>
+                    <tr>
+                        <th>NIS / NIP</th>
+                        <th>Status</th>
+                        <th>Keterangan</th>
+                        <th>Validasi</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <form action="" method="POST" enctype="multipart/form-data">
+                        <input type="hidden" name="$user_data['id_presensi']">
                         <?php
-                        $validate = '
-                    <input type="radio" id="not_yet" name="validate" value="NY">
-                    <label for="not_yet">NOT YET</label><br>
-                    <input type="radio" id="no" name="validate" value="N">
-                    <label for="no">NO</label><br>
-                    <input type="radio" id="yes" name="validate" value="Y">
-                    <label for="yes">YES</label>
-                    ';
                         while ($user_data = mysqli_fetch_array($query1)) {
                             echo "<tr>";
+                            $validate = '
+                            <input type="radio" id="not_yet" name="validate' . $user_data['id_presensi'] . '"value="NY">
+                            <label for="not_yet">NOT YET</label><br>
+                            <input type="radio" id="no" name="validate' . $user_data['id_presensi'] . '"value="N">
+                            <label for="no">NO</label><br>
+                            <input type="radio" id="yes" name="validate' . $user_data['id_presensi'] . '"value="Y">
+                            <label for="yes">YES</label>
+                            ';
+                            echo "<input type=\"hidden\" name=\"id_presensi" . $user_data['id_presensi'] . "\" value=\"" . $user_data['id_presensi'] . "\">";
                             echo "<td>" . $user_data['id_guru'] . "</td>";
                             echo "<td>" . $user_data['status'] . "</td>";
                             echo "<td> <a href=\"../berkas/" . $user_data['keterangan'] . "\" target=\"_blank\" rel=\"noopener noreferrer\">" . $user_data['keterangan'] . "</a> </td>";
-                            echo "<td>" . $user_data['valid'] . " |" . $validate . "</td>";
+                            echo "<td>" . $user_data['valid' . $user_data['id_presensi']] . " |" . $validate . "</td>";
                             echo "</tr>";
                         }
                         while ($user_data = mysqli_fetch_array($query2)) {
                             echo "<tr>";
+                            $validate = '
+                            <input type="radio" id="not_yet" name="validate' . $user_data['id_presensi'] . '"value="NY">
+                            <label for="not_yet">NOT YET</label><br>
+                            <input type="radio" id="no" name="validate' . $user_data['id_presensi'] . '"value="N">
+                            <label for="no">NO</label><br>
+                            <input type="radio" id="yes" name="validate' . $user_data['id_presensi'] . '"value="Y">
+                            <label for="yes">YES</label>
+                            ';
+                            echo "<input type=\"hidden\" name=\"id_presensi" . $user_data['id_presensi'] . "\" value=\"" . $user_data['id_presensi'] . "\">";
                             echo "<td>" . $user_data['id_siswa'] . "</td>";
                             echo "<td>" . $user_data['status'] . "</td>";
                             echo "<td> <a href=\"../berkas/" . $user_data['keterangan'] . "\" target=\"_blank\" rel=\"noopener noreferrer\">" . $user_data['keterangan'] . "</a> </td>";
@@ -95,10 +131,19 @@ $query2 = mysqli_query($koneksi, "SELECT * FROM presensi_siswa where tgl_presens
                             echo "</tr>";
                         }
                         ?>
-                    </tbody>
-                </table>
-                <input type="submit" name="submitBtn" value="Submit" class="submit" />
+                        <input type="submit" name="submitBtn" value="Submit" class="submit" />
+                </tbody>
+            </table>
+            <div>
+                <h3>
+                    <?php
+                    if (isset($alert)) {
+                        echo $alert;
+                    }
+                    ?>
+                </h3>
             </div>
+        </div>
         </form>
     </div>
 </body>
